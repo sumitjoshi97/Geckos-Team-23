abceb2fd76faffc8a0da64c67277b8ffe5c3cabc
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-01-09 12:08:44 +0530

    chore: add function to check word in local-storage

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -9,23 +9,27 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     fetch(URL)
       .then(response => response.json())
       .then(data => {
-        let definitionData = [];
-
-        for (let i = 0; i < 3; i++) {
-          if (data[i]) {
-            const response = { type: data[i].fl, def: data[i].shortdef[0] };
-            definitionData.push(response);
-          }
-        }
-
-        const newWord = {
-          word: request.word,
-          defs: definitionData,
-        };
-
         let wordList = JSON.parse(localStorage.getItem("wordList")) || [];
-        wordList = [newWord, ...wordList];
-        localStorage.setItem("wordList", JSON.stringify(wordList));
+
+        // check if word is in local-storage
+        if (isWordStored(request.word, wordList)) {
+          let definitionData = [];
+
+          for (let i = 0; i < 3; i++) {
+            if (data[i]) {
+              const response = { type: data[i].fl, def: data[i].shortdef[0] };
+              definitionData.push(response);
+            }
+          }
+
+          const newWord = {
+            word: request.word.toLowerCase(),
+            defs: definitionData,
+          };
+
+          wordList = [newWord, ...wordList];
+          localStorage.setItem("wordList", JSON.stringify(wordList));
+        }
 
         sendResponse({ response: `${data[0].fl}: ${data[0].shortdef[0]}` });
       })
@@ -34,6 +38,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   return true;
 });
 
-getWords = () => {
-  return JSON.parse(localStorage.getItem("wordList"));
+// function to check word is in local-storage or not
+isWordStored = (word, wordList) => {
+  for (let w in wordList) {
+    if (w.word === word.toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
 };
